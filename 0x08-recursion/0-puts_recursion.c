926ee723aed3972e05b1283a8b5ba55f53739027
Mwakanemela <kayangemwakanemela@gmail.com>
2023-04-04 23:52:28 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _puts_recursion - put recursion
+  * @s: string output
+  * Return: nothing
+  */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
